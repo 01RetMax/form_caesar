c57cb7de7cddc7591bf5b013b9aab44ab72fc386
01RetMax <01RetMax@rgtfo-me.it>
2018-03-06 11:14:36 +0100

    Verschlüsseln funktioniert noch nicht

--- form_caesar/form_caesar/Form1.Designer.cs
+++ form_caesar/form_caesar/Form1.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_ver = new System.Windows.Forms.TextBox();
+            this.tb_sch = new System.Windows.Forms.TextBox();
+            this.tb_ent = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.lbl_ver = new System.Windows.Forms.Label();
+            this.lbl_sch = new System.Windows.Forms.Label();
+            this.lbl_ent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_ver
+            // 
+            this.tb_ver.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tb_ver.Location = new System.Drawing.Point(264, 63);
+            this.tb_ver.Multiline = true;
+            this.tb_ver.Name = "tb_ver";
+            this.tb_ver.Size = new System.Drawing.Size(672, 93);
+            this.tb_ver.TabIndex = 0;
+            this.tb_ver.TextChanged += new System.EventHandler(this.tb_ver_TextChanged);
+            // 
+            // tb_sch
+            // 
+            this.tb_sch.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_sch.Location = new System.Drawing.Point(319, 199);
+            this.tb_sch.Name = "tb_sch";
+            this.tb_sch.Size = new System.Drawing.Size(73, 45);
+            this.tb_sch.TabIndex = 1;
+            this.tb_sch.TextChanged += new System.EventHandler(this.tb_sch_TextChanged);
+            // 
+            // tb_ent
+            // 
+            this.tb_ent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.tb_ent.Location = new System.Drawing.Point(264, 285);
+            this.tb_ent.Multiline = true;
+            this.tb_ent.Name = "tb_ent";
+            this.tb_ent.Size = new System.Drawing.Size(672, 93);
+            this.tb_ent.TabIndex = 2;
+            this.tb_ent.TextChanged += new System.EventHandler(this.tb_ent_TextChanged);
+            // 
+            // btn_1
+            // 
+            this.btn_1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(433, 180);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(503, 84);
+            this.btn_1.TabIndex = 3;
+            this.btn_1.Text = "Verschlüsseln <---> Entschlüsseln";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // lbl_ver
+            // 
+            this.lbl_ver.AutoSize = true;
+            this.lbl_ver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbl_ver.Location = new System.Drawing.Point(4, 98);
+            this.lbl_ver.Name = "lbl_ver";
+            this.lbl_ver.Size = new System.Drawing.Size(242, 29);
+            this.lbl_ver.TabIndex = 4;
+            this.lbl_ver.Text = "Verschlüsselter Text";
+            // 
+            // lbl_sch
+            // 
+            this.lbl_sch.AutoSize = true;
+            this.lbl_sch.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbl_sch.Location = new System.Drawing.Point(169, 209);
+            this.lbl_sch.Name = "lbl_sch";
+            this.lbl_sch.Size = new System.Drawing.Size(123, 29);
+            this.lbl_sch.TabIndex = 5;
+            this.lbl_sch.Text = "Schlüssel";
+            // 
+            // lbl_ent
+            // 
+            this.lbl_ent.AutoSize = true;
+            this.lbl_ent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbl_ent.Location = new System.Drawing.Point(5, 315);
+            this.lbl_ent.Name = "lbl_ent";
+            this.lbl_ent.Size = new System.Drawing.Size(241, 29);
+            this.lbl_ent.TabIndex = 6;
+            this.lbl_ent.Text = "Entschlüsselter Text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.lbl_ent);
+            this.Controls.Add(this.lbl_sch);
+            this.Controls.Add(this.lbl_ver);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.tb_ent);
+            this.Controls.Add(this.tb_sch);
+            this.Controls.Add(this.tb_ver);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_ver;
+        private System.Windows.Forms.TextBox tb_sch;
+        private System.Windows.Forms.TextBox tb_ent;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label lbl_ver;
+        private System.Windows.Forms.Label lbl_sch;
+        private System.Windows.Forms.Label lbl_ent;
     }
 }
 
--- form_caesar/form_caesar/Form1.cs
+++ form_caesar/form_caesar/Form1.cs
@@ -15,5 +15,88 @@ namespace form_caesar
         {
             InitializeComponent();
         }
+
+        private void tb_ver_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_sch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_ent_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 1; j++)
+            {
+                if (tb_ver.Text != null)
+                {
+                    Char[] chararray = tb_ver.Text.ToCharArray();
+                    int[] asciicode = new int[tb_ver.Text.Length];
+                    string ergebnis = null;
+
+                    for (int i = 0; i < tb_ver.Text.Length; i++)
+                    {
+                        asciicode[i] = Convert.ToInt32(chararray[i]);
+                    }
+
+                    for (int i = 0; i < tb_ver.Text.Length; i++)
+                    {
+                        asciicode[i] -= Convert.ToInt32(tb_sch.Text);
+                    }
+
+                    for (int i = 0; i < tb_ver.Text.Length; i++)
+                    {
+                        chararray[i] = Convert.ToChar(asciicode[i]);
+                    }
+
+                    for (int i = 0; i < tb_ver.Text.Length; i++)
+                    {
+                        ergebnis = ergebnis + Convert.ToString(chararray[i]);
+                    }
+
+                    tb_ent.Text = ergebnis;
+
+                    break;
+                }
+
+                if (tb_ent.Text != null)
+                {
+                    Char[] chararray = tb_ent.Text.ToCharArray();
+                    int[] asciicode = new int[tb_ent.Text.Length];
+                    string ergebnis = null;
+
+                    for (int i = 0; i < tb_ent.Text.Length; i++)
+                    {
+                        asciicode[i] = Convert.ToInt32(chararray[i]);
+                    }
+
+                    for (int i = 0; i < tb_ent.Text.Length; i++)
+                    {
+                        asciicode[i] += Convert.ToInt32(tb_sch.Text);
+                    }
+
+                    for (int i = 0; i < tb_ent.Text.Length; i++)
+                    {
+                        chararray[i] = Convert.ToChar(asciicode[i]);
+                    }
+
+                    for (int i = 0; i < tb_ent.Text.Length; i++)
+                    {
+                        ergebnis = ergebnis + Convert.ToString(chararray[i]);
+                    }
+
+                    tb_ver.Text = ergebnis;
+
+                    break;
+                }
+            }
+        }
     }
 }
